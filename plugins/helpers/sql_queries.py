45ad9750fc83cdf0b95c9914ab572cede28994ff
Fabien Beuret <fabien.beuret@delaware.pro>
2020-05-18 18:46:53 +0200

    add CREATE statements to queries

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,4 +1,102 @@
 class SqlQueries:
+    staging_songs_table_create = ("""
+        CREATE TABLE IF NOT EXISTS staging_songs (
+            num_songs int4,
+            artist_id varchar(256),
+            artist_name varchar(256),
+            artist_latitude numeric(18,0),
+            artist_longitude numeric(18,0),
+            artist_location varchar(256),
+            song_id varchar(256),
+            title varchar(256),
+            duration numeric(18,0),
+            "year" int4 );
+    """)
+
+    staging_events_table_create = ("""
+        CREATE TABLE IF NOT EXISTS staging_events (
+            artist varchar(256),
+            auth varchar(256),
+            firstname varchar(256),
+            gender varchar(256),
+            iteminsession int4,
+            lastname varchar(256),
+            length numeric(18,0),
+            "level" varchar(256),
+            location varchar(256),
+            "method" varchar(256),
+            page varchar(256),
+            registration numeric(18,0),
+            sessionid int4,
+            song varchar(256),
+            status int4,
+            ts int8,
+            useragent varchar(256),
+            userid int4 );
+    """)
+
+    user_table_create = ("""
+        CREATE TABLE public.users (
+            userid int4 NOT NULL,
+            first_name varchar(256),
+            last_name varchar(256),
+            gender varchar(256),
+            "level" varchar(256),
+            CONSTRAINT users_pkey PRIMARY KEY (userid) );
+    """)
+
+    artist_table_create = ("""
+        CREATE TABLE public.artists (
+            artistid varchar(256) NOT NULL,
+            name varchar(256),
+            location varchar(256),
+            latitude numeric(18,0),
+            longitude numeric(18,0) );
+    """)
+
+    song_table_create = ("""
+        CREATE TABLE public.songs (
+            songid varchar(256) NOT NULL,
+            title varchar(256),
+            artistid varchar(256),
+            "year" int4,
+            duration numeric(18,0),
+            CONSTRAINT songs_pkey PRIMARY KEY (songid) );
+    """)
+
+    time_table_create = ("""
+        CREATE TABLE public.time (
+            start_time timestamp NOT NULL,
+            "hour" int2,
+            "day" int2,
+            "week" int2,
+            "month" int2,
+            "year" int4,
+            dayofweek int2 );
+    """)
+
+    songplay_table_create = ("""
+        CREATE TABLE public.songplays (
+            playid varchar(32) NOT NULL,
+            start_time timestamp NOT NULL,
+            userid int4 NOT NULL,
+            "level" varchar(256),
+            songid varchar(256),
+            artistid varchar(256),
+            sessionid int4,
+            location varchar(256),
+            user_agent varchar(256),
+            CONSTRAINT songplays_pkey PRIMARY KEY (playid) );
+    """)
+
+    COPY_SQL = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        JSON 'auto';
+        """
+
     songplay_table_insert = ("""
         SELECT
                 md5(events.sessionid || events.start_time) songplay_id,
