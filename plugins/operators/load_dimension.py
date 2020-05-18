14ffd497b4b9b84b6b0d5e1229080522bf2c2a3a
Fabien Beuret <fabien.beuret@delaware.pro>
2020-05-18 18:48:36 +0200

    fix load_dimension

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -2,21 +2,45 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table,
+                 queries,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = redshift_conn_id
+        self.table = table
+        self.queries = queries
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        """
+        """
+        self.log.info(f'Starting LoadDimensionOperator for table {self.table}')
+
+        # get Redshift connection string
+        redshift_hook = PostgresHook(self.conn_id)
+
+        # create the corresponding table on Redshift
+        self.log.info(
+            f'Run CREATE statement for {self.table} table from helper class')
+        # the query is in the helper class
+        query = f'{self.table}_table_create'
+        sql_statement = getattr(self.queries, query)
+        self.log.info(sql_statement)
+        #redshift_hook.run(sql_statement)
+
+        # load dimension table from staging
+        self.log.info(
+            f'Run INSERT statement for {self.table} from helper class')
+        # the query is in the helper class
+        query = f'{self.table}_table_insert'
+        sql_statement = getattr(self.queries, query)
+        self.log.info(sql_statement)
+        #redshift_hook.run(sql_statement)
