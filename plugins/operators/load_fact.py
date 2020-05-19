a8da5f683f534ee3f2215c01d9412dbf551df342
Fabien Beuret <fabien.beuret@delaware.pro>
2020-05-19 18:08:15 +0200

    update & fix load_fact operator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -2,21 +2,42 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id,
+                 queries,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = redshift_conn_id
+        self.queries = queries
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        """
+        """
+        self.log.info('Starting LoadFactOperator for table songplays')
+
+        # get Redshift connection string
+        redshift_hook = PostgresHook(self.conn_id)
+
+        # create the corresponding table on Redshift
+        self.log.info(
+            f'Run CREATE statement for songplays table from helper class')
+        # the query is in the helper class
+        query = 'songplay_table_create'
+        sql_statement = getattr(self.queries, query)
+        self.log.info(sql_statement)
+        redshift_hook.run(sql_statement)
+
+        # load fact table from staging
+        self.log.info(f'Run INSERT statement for songplay from helper class')
+        # the query is in the helper class
+        query = 'songplay_table_insert'
+        sql_statement = getattr(self.queries, query)
+        self.log.info(sql_statement)
+        redshift_hook.run(sql_statement)
