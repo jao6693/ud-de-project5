3076280738618dcfa2407248bae95e107c0e9c01
Fabien Beuret <fabien.beuret@delaware.pro>
2020-05-20 15:09:44 +0200

    refactor data quality operator for dynamic checks

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -11,33 +11,46 @@ class DataQualityOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id,
                  table,
+                 queries,
+                 tests,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.conn_id = redshift_conn_id
         self.table = table
+        self.queries = queries
+        self.tests = tests
 
     def execute(self, context):
         self.log.info(f'Starting DataQualityOperator for table {self.table}')
 
+        # tests in error
+        error_count = 0
         # get Redshift connection string
         redshift_hook = PostgresHook(self.conn_id)
-        # count the number of rows
-        records = redshift_hook.get_records(
-            f'SELECT COUNT(*) FROM {self.table};')
-        if len(records) < 1 or len(records[0]) < 1:
-            self.log.info(
-                f'Data quality check failed. {self.table} returned no result')
-            raise ValueError(
-                f'Data quality check failed. {self.table} returned no result')
+        # get the check to run against Redshift
+        for test in self.tests:
+            sql_statement = getattr(self.queries, str(
+                test.get('check'))).format(self.table)
+            self.log.info(sql_statement)
+            records = redshift_hook.get_records(sql_statement)[0]
+            self.log.info(records[0])
 
-        num_records = records[0][0]
-        if num_records < 1:
-            self.log.info(
-                f'Data quality check failed. {self.table} contained 0 rows')
-            raise ValueError(
-                f'Data quality check failed. {self.table} contained 0 rows')
+            if test.get('operator') == '=':
+                if records[0] != test.get('result'):
+                    error_count += 1
+            if test.get('operator') == '>':
+                if records[0] <= test.get('result'):
+                    error_count += 1
+            if test.get('operator') == '<':
+                if records[0] >= test.get('result'):
+                    error_count += 1
 
-        # test passed !!
-        self.log.info(
-            f'Data quality on table {self.table} check passed with {records[0][0]} records')
+        if error_count == 0:
+            # tests passed !!
+            self.log.info(
+                f'Data quality on table {self.table} passed successfully')
+        else:
+            # tests failed
+            self.log.error(f'Data quality on table {self.table} failed')
+            raise ValueError(f'Data quality on table {self.table} failed')
